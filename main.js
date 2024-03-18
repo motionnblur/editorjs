09cf3cdd06677d46b904d6f48d3cbeb9a5961ee4
carbonfiberdev <carbonfiberdev@gmail.com>
2024-03-19 02:52:08 +0300

    some refactoring

--- main.js
+++ main.js
@@ -1,129 +1,47 @@
-/////// variables
-var _root;
-var currentSelectedSprite;
-var currentSpriteTemp;
-var offset = {
-  x: 0,
-  y: 0,
-};
-var spriteArray = [];
-var selectionAreaWidth = 40;
-var selectionArea = document.createElement("div");
-selectionArea.classList.add("selection-area");
-////// variables
+class Editor {
+  constructor() {
+    this.spriteArray = [];
+    this.currentSelectedSprite = null;
+    this.mouseOffsetFromSprite = {
+      x: 0,
+      y: 0,
+    };
+  }
+  SetMouseOffsetFromSprite(mosePosX, mousePosY, currentSpritePos) {
+    this.mouseOffsetFromSprite = {
+      x: mosePosX - currentSpritePos.x,
+      y: mousePosY - currentSpritePos.y,
+    };
+  }
+  GetMouseOffsetFromSprite() {
+    return this.mouseOffsetFromSprite;
+  }
+  SetCurrentSelectedSprite(sprite) {
+    this.currentSelectedSprite = sprite;
+  }
+  GetCurrentSelectedSprite() {
+    return this.currentSelectedSprite;
+  }
+  ClearCurrentSelectedSprite() {
+    this.currentSelectedSprite = null;
+  }
+  AddSpriteToArray(sprite) {
+    this.spriteArray.push(sprite);
+  }
+  RemoveSpriteFromArray(sprite) {
+    this.spriteArray = this.spriteArray.filter((s) => s !== sprite);
+  }
 
-document.addEventListener("DOMContentLoaded", function (event) {
-  _root = document.getElementById("root");
-  _root.addEventListener("mousedown", onMouseDownRoot);
-  _root.addEventListener("mouseup", onMouseUpRoot);
-  _root.addEventListener("mousemove", onMouseMoveRoot);
-  window.addEventListener("keydown", onKeyDownRoot);
-});
-
-function dropHandler(ev) {
-  console.log("File(s) dropped");
-  ev.preventDefault();
-
-  if (ev.dataTransfer.items) {
-    [...ev.dataTransfer.items].forEach((item, i) => {
-      if (item.kind === "file" && item.type === "image/png") {
-        const file = item.getAsFile();
-
-        const sprite = new Sprite(file, 50, 50, {
-          x: ev.clientX,
-          y: ev.clientY,
-        });
-        spriteArray.push(sprite);
-
-        //currentSelectedSprite = sprite;
-      }
-    });
-  } else {
-    [...ev.dataTransfer.files].forEach((file, i) => {
-      //
-    });
+  onMouseDownCallback(e, obj) {
+    this.SetMouseOffsetFromSprite(
+      e.clientX,
+      e.clientY,
+      obj.GetSpritePosition()
+    );
+    this.SetCurrentSelectedSprite(obj);
   }
 }
 
-function onKeyDownRoot(e) {
-  console.log("keydown");
-  if (e.key === "Delete") {
-    if (currentSpriteTemp) {
-      currentSpriteTemp.remove();
-      if (selectionArea) {
-        selectionArea.style.display = "none";
-      }
-    }
-  }
-}
-function dragOverHandler(ev) {
-  ev.preventDefault();
-}
-function onMouseDownRoot(e) {
-  console.log("mouse down root page");
-  if (currentSpriteTemp && !isMouseOnSprite(e)) {
-    currentSpriteTemp = null;
-    selectionArea.style.display = "none";
-  }
-}
-function onMouseUpRoot() {
-  console.log("mouse up root page");
-  selectionArea.style.zIndex = 1;
-  currentSelectedSprite.image.style.zIndex = 5;
-  currentSelectedSprite = null;
-}
-
-function onMouseDownSprite(e) {
-  /* currentSelectedSprite = e.target;
-  currentSelectedSprite.image.style.zIndex = 100;
-
-  currentSpriteTemp = currentSelectedSprite;
-  const leftInt = parseInt(this.style.left);
-  const topInt = parseInt(this.style.top);
-  offset = {
-    x: e.clientX - leftInt,
-    y: e.clientY - topInt,
-  };
-
-  addSelectionAreaToSprite(currentSpriteTemp); */
-}
-function onMouseMoveRoot(e) {
-  if (currentSelectedSprite) {
-    currentSelectedSprite.image.style.left = e.clientX - offset.x + "px";
-    currentSelectedSprite.image.style.top = e.clientY - offset.y + "px";
-    if (selectionArea) {
-      selectionArea.style.left =
-        e.clientX - offset.x - selectionAreaWidth / 2 + "px";
-      selectionArea.style.top =
-        e.clientY - offset.y - selectionAreaWidth / 2 + "px";
-    }
-  }
-}
-
-function isMouseOnSprite(e) {
-  return (
-    e.clientX >= currentSpriteTemp.offsetLeft &&
-    e.clientX <= currentSpriteTemp.offsetLeft + currentSpriteTemp.offsetWidth &&
-    e.clientY >= currentSpriteTemp.offsetTop &&
-    e.clientY <= currentSpriteTemp.offsetTop + currentSpriteTemp.offsetHeight
-  );
-}
-function addSelectionAreaToSprite() {
-  if (selectionArea) {
-    selectionArea.style.display = "block";
-    selectionArea.style.zIndex = 99;
-  }
-  selectionArea.style.left =
-    currentSpriteTemp.offsetLeft - selectionAreaWidth / 2 + "px";
-  selectionArea.style.top =
-    currentSpriteTemp.offsetTop - selectionAreaWidth / 2 + "px";
-  selectionArea.style.width =
-    currentSpriteTemp.offsetWidth + selectionAreaWidth + "px";
-  selectionArea.style.height =
-    currentSpriteTemp.offsetHeight + selectionAreaWidth + "px";
-  selectionArea.style.position = "absolute";
-  _root.appendChild(selectionArea);
-}
 class Sprite {
   constructor(file, width, height, pos) {
     this.AssignToDom(file, width, height, pos);
@@ -148,8 +66,14 @@ class Sprite {
     this.image = img;
     src.appendChild(img);
   }
-  onMouseDownCallback() {
-    currentSelectedSprite = this;
+  GetSpritePosition() {
+    return {
+      x: parseInt(this.image.style.left),
+      y: parseInt(this.image.style.top),
+    };
+  }
+  onMouseDownCallback(e) {
+    _editor.onMouseDownCallback(e, this);
   }
 }
 const TextureMixin = {
@@ -164,7 +88,85 @@ const EventObjectMixin = {
   constructor() {
     this.image.addEventListener("mousedown", this.onMouseDown.bind(this));
   },
-  onMouseDown() {
-    this.onMouseDownCallback();
+  onMouseDown(e) {
+    this.onMouseDownCallback(e);
   },
 };
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////-->CLASSES
+
+/////// variables
+var _root;
+var offset = {
+  x: 0,
+  y: 0,
+};
+var _editor = new Editor();
+////// variables
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  _root = document.getElementById("root");
+  _root.addEventListener("mousedown", onMouseDownRoot);
+  _root.addEventListener("mouseup", onMouseUpRoot);
+  _root.addEventListener("mousemove", onMouseMoveRoot);
+  window.addEventListener("keydown", onKeyDownRoot);
+});
+
+function dropHandler(ev) {
+  console.log("File(s) dropped");
+  ev.preventDefault();
+
+  if (ev.dataTransfer.items) {
+    [...ev.dataTransfer.items].forEach((item, i) => {
+      if (item.kind === "file" && item.type === "image/png") {
+        const file = item.getAsFile();
+
+        const sprite = new Sprite(file, 50, 50, {
+          x: ev.clientX,
+          y: ev.clientY,
+        });
+        _editor.AddSpriteToArray(sprite);
+      }
+    });
+  } else {
+    [...ev.dataTransfer.files].forEach((file, i) => {
+      //
+    });
+  }
+}
+////////////////////////////////////////////////////////////////////////////////--> MAIN DOM EVENTS
+function onKeyDownRoot(e) {
+  console.log("keydown");
+  if (e.key === "Delete") {
+    if (currentSpriteTemp) {
+      currentSpriteTemp.remove();
+      if (selectionArea) {
+        selectionArea.style.display = "none";
+      }
+    }
+  }
+}
+
+function onMouseDownRoot(e) {
+  ///
+}
+function onMouseUpRoot() {
+  if (_editor.GetCurrentSelectedSprite()) {
+    _editor.GetCurrentSelectedSprite().image.style.zIndex = 5;
+    _editor.ClearCurrentSelectedSprite();
+  }
+}
+
+function onMouseMoveRoot(e) {
+  if (_editor.GetCurrentSelectedSprite()) {
+    _editor.GetCurrentSelectedSprite().image.style.left =
+      e.clientX - offset.x + "px";
+    _editor.GetCurrentSelectedSprite().image.style.top =
+      e.clientY - offset.y + "px";
+  }
+}
+////////////////////////////////////////////////////////////////////////////////--> MAIN DOM EVENTS
+
+function dragOverHandler(ev) {
+  ev.preventDefault();
+}
